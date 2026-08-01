a771b15bba24f17df8b46e8b9207e9b5113cbf5f
diogotito <diogotitomarques@gmail.com>
2026-08-01 13:58:06 +0100

    Upgrade to nixos-26.05
    Hello again, NixOS

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs-old.url = "github:nixos/nixpkgs/nixos-25.05";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-old2.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-old.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # home-manager = {
@@ -21,7 +22,11 @@
     nixpkgs-unstable = import inputs.nixpkgs-unstable {
       inherit system;
       config.allowUnfree = true;
+      # Sublime Text 4
       config.permittedInsecurePackages = ["openssl-1.1.1w"]; # required for Sublime Text 4
+      config.problems.handlers = {
+        sublimetext4.broken = "warn"; # or "ignore"
+      };
     };
   in {
     nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
